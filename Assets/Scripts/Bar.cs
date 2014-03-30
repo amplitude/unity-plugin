68b98709bec769906d784dd3e9a5c2b586dbd76c
Curtis <curtis@sonalight.com>
2014-03-30 14:51:44 -0700

    Update demo scripts

--- Assets/Scripts/Bar.cs
+++ Assets/Scripts/Bar.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Bar : MonoBehaviour {
 
@@ -20,7 +21,21 @@ public class Bar : MonoBehaviour {
 
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
 		if(GUI.Button(new Rect(20,40,80,20), "Level 1")) {
-			Amplitude.Instance.logEvent("tapped");
+			Amplitude amplitude = Amplitude.Instance;
+			amplitude.logEvent("tapped");
+			Dictionary<string, object> userProperties = new Dictionary<string, object>()
+			{
+				{"float_gprop", 1.0}
+			};
+			amplitude.setUserProperties(userProperties);
+			
+			Dictionary<string, object> demoOptions = new Dictionary<string, object>()
+			{
+				{"Bucket" , "A" },
+				{"Credits" , 9001}
+			};
+			amplitude.logEvent("unity event 2", demoOptions);
+			amplitude.logRevenue(0.03);
 		}
 	}
 }
