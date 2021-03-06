84c24caa07c95c05e92952e064d15f33f9d6ee5c
Dante Tam <dante.tam1@gmail.com>
2021-03-05 23:22:04 -0800

    Separate logic between GPS and IDFA, clean up PR

---
+++ IdfaIOS/CustomGps.m
@@ -0,0 +1,76 @@
+#import <CoreLocation/CoreLocation.h>
+
+//Please ensure this path points to the file 'Assets/Plugins/iOS/Amplitude/Amplitude.h'
+#import "../../Plugins/iOS/Amplitude/Amplitude.h"
+
+#import "../../Plugins/iOS/Amplitude/AmplitudeCWrapper.h"
+
+typedef NSDictionary* (^AMPLocationInfoBlock)(void);
+
+@interface CustomLocationManager : NSObject<CLLocationManagerDelegate> 
+@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, strong) NSString *instanceName;
+@end
+
+@implementation CustomLocationManager 
+
+- initWithString:(NSString *)instanceNameString
+{
+    self = [super init];
+    if (self) {
+        self.instanceName = instanceNameString;
+    }
+    return self;
+}
+
+- (void)currentLocationIdentifier 
+{
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    if ([CLLocationManager locationServicesEnabled] && 
+            [CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+        self.locationManager.distanceFilter = 500; 
+        [self.locationManager startUpdatingLocation];
+    }
+    else {
+        [self.locationManager requestWhenInUseAuthorization];
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus *)status 
+{
+    if ([CLLocationManager locationServicesEnabled] && status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+        self.locationManager.distanceFilter = 500; 
+        [self.locationManager startUpdatingLocation];
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations 
+{
+    CLLocation* location = [locations lastObject];
+    float lat = location.coordinate.latitude;
+    float lng = location.coordinate.longitude;
+    AMPLocationInfoBlock locationInfoBlock = ^{
+        return @{
+            @"lat" : [NSNumber numberWithFloat:lat],
+            @"lng" : [NSNumber numberWithFloat:lng]
+        };
+    };
+    [[Amplitude instanceWithName:self.instanceName] setLocationInfoBlock:locationInfoBlock];
+}
+
+@end
+
+static CustomLocationManager *customLocationManager = nil;
+
+#pragma mark - Functions Exposed to C#
+
+void setLocationInfoBlock(const char* instanceName) 
+{
+    if (!customLocationManager) {
+        customLocationManager = [[CustomLocationManager alloc] initWithString:ToNSString(instanceName)];
+    }
+    [customLocationManager currentLocationIdentifier];
+}
